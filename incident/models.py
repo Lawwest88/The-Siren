1c48b9e1d8e9360705be7dc1d65640ea03aa3acb
Lawwest88 <lawrence-west@hotmail.com>
2022-03-26 19:34:46 -0500

    migrated models

--- incident/models.py
+++ incident/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ServiceReq(models.Model):
+    #services requested
+    fire = models.CharField(max_length=50)
+    police = models.CharField(max_length=50)
+    medical = models.CharField(max_length=50)
+    PublicWorks = models.CharField(max_length=50)
+
+    #image 
+    image = models.ImageField(default=None)
--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Reporter(models.Model):
+
+    #user info
+    first_name  = models.CharField(max_length=100)
+    last_name   = models.CharField(max_length=100)
+    User_email  = models.EmailField(max_length=125,blank=True,unique=True)
+    age = models.IntegerField(null=True)
